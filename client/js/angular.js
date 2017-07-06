385b626b39cf3e2d113688361a66b5a2259cc916
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2017-07-05 23:39:50 -0300

    mostrando funcionario, departamento e dependente

---
+++ client/js/angular.js
@@ -0,0 +1,36 @@
+angular.module('bd2Companhia', [])
+  .controller('companhiaController', ($scope, $http) => {
+    $scope.tabActive = 'funcionario';
+    $scope.ativarTab = function (tab) {
+      if ($scope.tabActive != tab)
+        $scope.tabActive = tab;
+    }
+    // Get funcionário all todos
+    $scope.funcionario = [];
+    $http({
+      method: 'GET',
+      url: '/api/v1/funcionario/all'
+    }).then(function successCallback(response) {
+      $scope.funcionario = response && response.data;
+    }, function errorCallback(response) {
+      console.log('Error: ' + response);
+    });
+    $scope.departamento = [];
+    $http({
+      method: 'GET',
+      url: '/api/v1/departamento/all'
+    }).then(function successCallback(response) {
+      $scope.departamento = response && response.data;
+    }, function errorCallback(response) {
+      console.log('Error: ' + response);
+    });
+    $scope.dependente = [];
+    $http({
+      method: 'GET',
+      url: '/api/v1/dependente/all'
+    }).then(function successCallback(response) {
+      $scope.dependente = response && response.data;
+    }, function errorCallback(response) {
+      console.log('Error: ' + response);
+    });
+  });
